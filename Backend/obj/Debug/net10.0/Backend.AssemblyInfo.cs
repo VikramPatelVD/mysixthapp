1788986fe5a1184b3f5b29faf05d88c6e6bf028a
VikramPatelVD <vd_patel@yahoo.com>
2026-06-03 00:30:05 +0530

    2

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b956172b72e431dc5297f51e48f9f1ee659db562")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e98721953700318e4faf0d79654a1dba0b8da4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
