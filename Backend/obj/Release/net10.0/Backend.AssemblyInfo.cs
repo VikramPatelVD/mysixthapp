54084ff12d16d6beefa439cf451b11d6dcd0ef3d
VikramPatelVD <vd_patel@yahoo.com>
2026-06-04 18:34:56 +0530

    4

--- Backend/obj/Release/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Release/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1788986fe5a1184b3f5b29faf05d88c6e6bf028a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7804f467b234a216b2a1d7477d6bf63bfe37e08")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
